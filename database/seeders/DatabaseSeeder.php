c87a7f3e6589362500d55c720dc28153c75c7fb1
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2024-11-27 15:15:27 -0300

    Migrations, Factories e Seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Car;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        Car::factory()->count(200)->create();
+    }
+}
